eb112b336655d26b61b12a3d397951c044908bc9
iamnotmega <iamnotmega@proton.me>
2025-12-05 22:20:19 +0200

    Add 'cd' built-in command support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 use std::io::{stdin, stdout, Write};
+use std::env;
+use std::path::Path;
 
 fn main() {
     loop {
@@ -17,13 +19,25 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        // Run the command inserted by the user
-        let mut child = Command::new(command)
-            .args(args)
-            .spawn()
-            .unwrap();
+        match command { // Handle shell built-ins
+            "cd" => { // cd builtin
+                // Default to '/' if a directory was not provided
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) { // Set current directory  (or / if unspecified)
+                    eprintln!("{}", e); // Throw error on failure
+                }
+            },
+            command => { // Normal not built-in command
+                // Run the command inserted by the user
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
 
-        // Wait until previous command has been executed before accepting another
-        let _ = child.wait();
+                // Wait until previous command has been executed before accepting another
+                let _ = child.wait();
+            }
+        }
     }
 }
