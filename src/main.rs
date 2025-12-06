379d5eb9d7fcc5204a3c0709c13e323e3a3cfef6
iamnotmega <iamnotmega@proton.me>
2025-12-06 11:04:58 +0200

    Improve shell prompt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{stdout, stdin, Write};
+use std::env;
+use whoami;
 mod input;
 mod commands;
 
@@ -8,9 +10,13 @@ fn main(){
         // Check for CLI flags
         commands::commands();
 
-        // Use '>' character as the shell prompt
-        // Flush it to ensure it prints before read_line
-        print!("> ");
+        let user = whoami::username(); // Current logged in user
+        let host = whoami::fallible::hostname().unwrap_or_else(|_| "unknown".to_string()); // Device hostname
+        let dir = env::current_dir().unwrap(); // Current directory
+
+        // Print prompt with username, hostname and current directory
+        // Flush output to ensure it prints before read_line
+        print!("{}@{} {} > ", user, host, dir.display());
         let _ = stdout().flush();
 
         // Read user input
